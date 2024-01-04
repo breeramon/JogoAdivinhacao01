35ab20fde53bd68302e0569e96a5e978ec514b6d
breeramon <117483744+breeramon@users.noreply.github.com>
2024-01-04 14:34:58 +0000

    feito por tentativas

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
 from random import randint
 
-adivinhar = int(input("Escolha um número entre 0 e 10: "))
+numero_adivinhado = randint(0, 10)
+numero_tentativas = 3
 
-numero_aleatorio = randint(0, 10)
-
-if (adivinhar == numero_aleatorio):
-  print(f"Número sorteado: {numero_aleatorio}")
-  print(adivinhar)
-  print("ACERTOU!")
-else:
-  print(adivinhar)
-  print("ERROU!")
-  print("")
-
-while (adivinhar != numero_aleatorio):
-  adivinhar = int(input("Escolha um número entre 0 e 10: "))
-  if (adivinhar == numero_aleatorio):
-    print(f"Número sorteado: {numero_aleatorio}")
-    print(f"Seu número {adivinhar}")
-    print("ACERTOU!")
+for tentativa in range(1, numero_tentativas + 1):
+  numero_escolhido = int(input("Escolha um número entre 0 e 10: "))
+  if numero_escolhido == numero_adivinhado:
+    print(
+        f"Parabéns, você acertou em {tentativa} tentativas, o número era {numero_adivinhado}"
+    )
     break
   else:
-    print(adivinhar)
-    print("ERROU!")
-    print("")
+    print("Você errou!")
+print(f"O número era {numero_adivinhado}")
